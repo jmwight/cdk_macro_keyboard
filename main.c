0bc9c0a0b44eb59383d541366098b3af6381066a
John Morgan Wight <john.morgan.wight@proton.me>
2026-07-11 23:03:49 -0400

    Remove unnecessary setup() and loop() functions. They are needed in arduino programming not c.

--- main.c
+++ main.c
@@ -26,24 +26,6 @@ int main(void)
   }
 }
 
-// the setup function runs once when you press reset or power the board
-/*void setup()
-{
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  /* initialize 12 keys 
-  for(int i = 1; i < 13; i++)
-  {
-    pinMode(i, INPUT_PULLUP);
-  }
-}*/
-
-// the loop function runs over and over again forever
-void loop()
-{
-  
-}
-
 /* books a certain number of lines */
 void read_line(int lines)
 {
